208423105b7f17f39cf3236ed71b2c3c998890a3
Hank Donnay <hdonnay@gmail.com>
2014-09-05 14:43:41 -0500

    problem five (brute force)

--- c/src/five/five.c
+++ c/src/five/five.c
@@ -3,5 +3,17 @@
 
 void
 main() {
-	exits(0);
+	int i,j,x;
+	x = 0;
+	for(i=2;;i+=2,x=0){
+		for(j=1;j<=20;j++){
+			if(i%j != 0)
+				x++;
+		}
+		if(x == 0) {
+			print("%d\n", i);
+			exits(0);
+		}
+	}
+	exits("bad");
 }
