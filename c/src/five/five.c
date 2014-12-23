e302ba52e796c3563c183f461201152b5c859e59
Hank Donnay <hdonnay@gmail.com>
2014-12-23 15:14:30 -0600

    speed up five

--- c/src/five/five.c
+++ c/src/five/five.c
@@ -3,16 +3,15 @@
 
 void
 main() {
-	int i,j,x;
-	x = 0;
-	for(i=2;;i+=2,x=0){
+	int i, j;
+	for(i=2;;i+=2){
 		for(j=1;j<=20;j++){
 			if(i%j != 0)
-				x++;
-		}
-		if(x == 0) {
-			print("%d\n", i);
-			exits(0);
+				break;
+			if(j == 20) {
+				print("%d\n", i);
+				exits(0);
+			}
 		}
 	}
 	exits("bad");
